c0a2e24e9874adc7e3012cffd320c18f4a814d1f
Alldo Faiz Ramadhani <75169100+alldofaiz14122000@users.noreply.github.com>
2024-06-16 23:36:15 +0700

    Move DB to Posgree

--- backend/settings.py
+++ backend/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'laundryDB',
+        'USER': 'postgres',
+        'PASSWORD': '14122000',
+        'HOST': 'localhost',
+        'PORT': '5432',  # or your PostgreSQL port
     }
 }
 
---
+++ laundryappapi/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from .models import Order, LaundryItem
+
+class LaundryItemForm(forms.ModelForm):
+    class Meta:
+        model = LaundryItem
+        fields = ['jenis_laundry', 'jumlah_berat']
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = [
+            'hp', 'nama', 'tanggal_masuk', 'tanggal_selesai',
+            'total_harga', 'catatan_khusus', 'status'
+        ]
+
+    def clean_nama(self):
+        nama = self.cleaned_data.get('nama')
+        # Tambahkan validasi atau modifikasi di sini jika diperlukan
+        return nama
+
+    def clean_hp(self):
+        hp = self.cleaned_data.get('hp')
+        # Validasi khusus untuk nomor HP, contoh: panjang atau format nomor
+        if not hp.isdigit():
+            raise forms.ValidationError("Nomor HP harus terdiri dari angka.")
+        return hp
